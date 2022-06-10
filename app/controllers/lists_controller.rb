3216f7195b6cb9d6ff14a6845b51bf42c5362b20
EC2 Default User <ec2-user@ip-172-31-46-216.ap-northeast-1.compute.internal>
2022-06-10 10:24:07 +0000

    4章での更新

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,13 +7,26 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)  
+  end
+  
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id]) 
+    List.find(1)
+    List.find(2)
+  end
+
+  def edit
   end
   
   
   
   private 
-  def list_params　
+  def list_params
     params.require(:list).permit(:title, :body)
   end
 end
